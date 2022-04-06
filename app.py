4f165f4a2fdbe1d0b646812a5d269fbe45da97f1
Akarsh L <102985186+akarshpyr@users.noreply.github.com>
2022-04-06 17:11:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+a = list(range(1,501))
+list2 = []
+for i in a:
+    if (i**0.5)%3 == 0:
+        list2.append(int(i**0.5))
+print(list2)
+#list of roots till 500 which are divisible by 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
