fd14e8442899973cc4d617dd74bc62b9edfcbdf9
Akarsh L <102985186+akarshpyr@users.noreply.github.com>
2023-05-20 11:47:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
-a = list(range(1,501))
-list2 = []
-for i in a:
-    if (i**0.5)%3 == 0:
-        list2.append(int(i**0.5))
-print(list2)
-#list of roots till 500 which are divisible by 3
+from flask import Flask,render_template,request
+import joblib
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+mul_reg = open("Profit_predictor.pkl", "rb")
+ml_model = joblib.load(mul_reg)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/predict", methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        try:
+            Administration = float(request.form['Administration'])
+            MRRD = float(request.form['MRRD'])
+            pred_args = [Administration, MRRD]
+            pred_args_arr = np.array(pred_args)
+            pred_args_arr = pred_args_arr.reshape(1, -1)
+            model_prediction = ml_model.predict(pred_args_arr)
+            model_prediction = round(float(model_prediction), 2)
+        except ValueError:
+            return "Please check for the incorrect values"
+    return render_template('predict.html', prediction = model_prediction)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    app.run(debug=True)
